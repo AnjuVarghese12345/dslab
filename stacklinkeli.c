086aa388893049c0d0880cddb001660804f51cd5
AnjuVarghese12345 <93638518+AnjuVarghese12345@users.noreply.github.com>
2021-12-14 23:01:29 +0530

    Update stacklinkeli.c

--- stacklinkeli.c
+++ stacklinkeli.c
@@ -1,86 +1,119 @@
-#include<stdio.h>
-#include<conio.h>
-#include<malloc.h>
-#include<process.h>
-struct stack
-{
-int no;
-struct stack *next;
-}
-*start= NULL;
-typedef struct stack st;
-void main()
-{
-void push();
-int pop();
-void display();
-char ch
-int choice
-int item;
-do
-{
-clrscr();
-printf("1. PUSH \n");
-
-printf("2. POP \n");
-
-printf("3. TRAVERSE \n");
-
-printf("Enter your choice \n");
-scanf("%d",&choice);
-switch (choice)
-{
-case 1: 
- push(); 
- break;
-case 2:
- item=pop( )
- printf("The deleted element is",item); 
- break;
-case 3:
- display();
- break;
-default:printf("Wrong choice");
-};
-prinf("do you want to continue(y/n) \n");
-fflush(stdin);
-scanf("%d",&ch);
-}
-while ((ch='Y') || (ch = 'y'));
-}
-void push()
-{
-st *node;
-node = new(st);
-printf("Enter the number to be insert");
-scanf("%d",&node->no) ;
-node->next = start;
-start = node;
-}
-int pop()
-{
-st temp;
-temp = start;
-if (start == NULL)
-printf("stack is already empty");
-getch();
-exit(0);
-else
-{
-start= start->next;
-delete(temp); 
-}
-return (temp->no);
-}
-void display()
-{
-st *temp;
-temp = start; 
-while(temp->next != NULL)
-{
-    printf("no=" << temp->no); 
-    temp = temp->next;
-}
-printf("no", temp→no);
-}
-}
+#include <stdio.h>  
+#include <stdlib.h>  
+void push();  
+void pop();  
+void display();  
+struct node   
+{  
+int val;  
+struct node *next;  
+};  
+struct node *head;  
+  
+void main ()  
+{  
+    int choice=0;     
+    printf("\n*********Stack operations using linked list*********\n");  
+    printf("\n----------------------------------------------\n");  
+    while(choice != 4)  
+    {  
+        printf("\n\nChose one from the below options...\n");  
+        printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");  
+        printf("\n Enter your choice \n");        
+        scanf("%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            {   
+                push();  
+                break;  
+            }  
+            case 2:  
+            {  
+                pop();  
+                break;  
+            }  
+            case 3:  
+            {  
+                display();  
+                break;  
+            }  
+            case 4:   
+            {  
+                printf("Exiting....");  
+                break;   
+            }  
+            default:  
+            {  
+                printf("Please Enter valid choice ");  
+            }   
+    };  
+}  
+}  
+void push ()  
+{  
+    int val;  
+    struct node *ptr = (struct node*)malloc(sizeof(struct node));   
+    if(ptr == NULL)  
+    {  
+        printf("not able to push the element");   
+    }  
+    else   
+    {  
+        printf("Enter the value");  
+        scanf("%d",&val);  
+        if(head==NULL)  
+        {         
+            ptr->val = val;  
+            ptr -> next = NULL;  
+            head=ptr;  
+        }   
+        else   
+        {  
+            ptr->val = val;  
+            ptr->next = head;  
+            head=ptr;  
+               
+        }  
+        printf("Item pushed");  
+          
+    }  
+}  
+  
+void pop()  
+{  
+    int item;  
+    struct node *ptr;  
+    if (head == NULL)  
+    {  
+        printf("Underflow");  
+    }  
+    else  
+    {  
+        item = head->val;  
+        ptr = head;  
+        head = head->next;  
+        free(ptr);  
+        printf("Item popped");  
+          
+    }  
+}  
+void display()  
+{  
+    int i;  
+    struct node *ptr;  
+    ptr=head;  
+    if(ptr == NULL)  
+    {  
+        printf("Stack is empty\n");  
+    }  
+    else  
+    {  
+        printf("Printing Stack elements \n");  
+        while(ptr!=NULL)  
+        {  
+            printf("%d\n",ptr->val);  
+            ptr = ptr->next;  
+        }  
+    }  
+}  
